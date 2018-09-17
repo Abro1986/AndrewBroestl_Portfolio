01835b4443bc4504bedae00ed281d5d7a3d0d7b5
Abro1986 <AndrewBroestl@gmail.com>
2018-09-16 19:14:07 -0600

    finally added links to unfinished capstone project

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 console.log('working');
+let localHost = 'http:localhost:8080/api/mail'
+let deployed = 'https://fathomless-fortress-45044.herokuapp.com/api/mail'
 
 $('#mailform').submit(function (e) {
 
 	e.preventDefault();
 	$.ajax({
-		url: 'https://fathomless-fortress-45044.herokuapp.com/api/mail',
+		url: deployed,
 		type: 'POST',
 		data: $(this).serialize(),
 		success: function(data) {
